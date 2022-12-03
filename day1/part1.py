f30a9abbf3515b86212c24309be655d094ac0a6f
Kemal Yildirim <kemalyildirm@gmail.com>
2022-12-03 04:20:10 +0300

    day compl

---
+++ day1/part1.py
@@ -0,0 +1,37 @@
+
+data = None
+test_data = """
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+
+with open("data") as f:
+    data = f.read()
+
+def solution(data):
+    dataArr = data.split("\n")
+    result = []
+    tmp = 0
+    for line in dataArr:
+        if line != '':
+            tmp += int(line)
+        else:
+            result.append(tmp)
+            tmp = 0
+    return max(result)
+
+
+
+print(solution(data))
